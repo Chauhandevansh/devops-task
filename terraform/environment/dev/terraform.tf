d3138581f8cb6efe405c6223fd53abb56de6d29d
Chauhandevansh <chauhandevansh04@gmail.com>
2025-09-18 10:16:14 +0530

    terraform required_version added

--- terraform/environment/dev/terraform.tf
+++ terraform/environment/dev/terraform.tf
@@ -6,6 +6,7 @@ terraform {
       version = "6"
     }
   }
+  required_version = ">= 1.3.0"
   backend "s3" {
     bucket         = "swayatt-backend-state-remote-bucket"
     key            = "dev/terraform.tfstate"
